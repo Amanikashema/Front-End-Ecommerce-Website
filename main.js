9546e0fcf96ec33f26a5b835fcd8878c8e6c72f1
Your Name <amanikashema000@gmail.com>
2021-03-09 16:22:26 +0200

    updates

--- main.js
+++ main.js
@@ -202,3 +202,14 @@ window.onclick = function (event) {
     modal.style.display = "none";
   }
 };
+
+// Hamburger section for the navbar
+
+function hamBurger() {
+  const navs = document.querySelectorAll(".Navbar__Items");
+
+  navs.forEach((nav) => nav.classList.toggle("Navbar__ToggleShow"));
+}
+
+document.querySelector('.Navbar__Link-toggle')
+  .addEventListener('click', classToggle);
